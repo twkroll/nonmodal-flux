56d1cc30e3d9f8d414d6e2d89a271a17042dccae
twkroll <timmykroll@googlemail.com>
2026-09-01 20:22:08 +0200

    models: add application model package

---
+++ src/nonmodal_flux/models/__init__.py
@@ -0,0 +1,5 @@
+"""Physics-derived application model constructors."""
+
+from .hasegawa_wakatani import hasegawa_wakatani_matrices, make_hasegawa_wakatani_problem
+
+__all__ = ["hasegawa_wakatani_matrices", "make_hasegawa_wakatani_problem"]
