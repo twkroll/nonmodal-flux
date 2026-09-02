321b80046a6ff331cfc027814f8d90de947febcf
twkroll <timmykroll@googlemail.com>
2026-09-02 08:20:00 +0200

    model: export D10 zonal-flow constructors

--- src/nonmodal_flux/models/__init__.py
+++ src/nonmodal_flux/models/__init__.py
@@ -1,5 +1,14 @@
 """Physics-derived application model constructors."""
 
 from .hasegawa_wakatani import hasegawa_wakatani_matrices, make_hasegawa_wakatani_problem
+from .hasegawa_wakatani_zonal_flow import (
+    hasegawa_wakatani_zonal_flow_matrices,
+    make_hasegawa_wakatani_zonal_flow_problem,
+)
 
-__all__ = ["hasegawa_wakatani_matrices", "make_hasegawa_wakatani_problem"]
+__all__ = [
+    "hasegawa_wakatani_matrices",
+    "hasegawa_wakatani_zonal_flow_matrices",
+    "make_hasegawa_wakatani_problem",
+    "make_hasegawa_wakatani_zonal_flow_problem",
+]
